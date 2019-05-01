5350101a3da007e27d31fc731a200fc18f1cb693
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-05-01 18:00:04 -0500

    Create deck.cpp

---
+++ deck.cpp
@@ -0,0 +1,20 @@
+#ifndef DECK_CPP
+#define DECK_CPP
+
+#include <string>
+#include <cstdlib>
+#include <card.cpp>
+
+using namespace std;
+
+class deck{
+	
+	private:
+		int size=52;
+		
+	public:
+	
+		void shuffle(){
+		}
+};
+#endif
